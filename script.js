8fb992e9cc9a6c0319faddb8e08a5515707dc2a5
Pain <holtbrenden@yahoo.com>
2022-03-17 17:10:09 -0600

    finally done

--- script.js
+++ script.js
@@ -4,21 +4,21 @@ var quest = [
     pick: ["The number one hyperactive, knuckle headed ninja" ,
      "A side character",
      "In love with TenTen"] ,
-    answer: 1
+    answer: "The number one hyperactive, knuckle headed ninja"
     },
 
     {question: "How many jutsu did Kakashi copy ?" ,
     pick: ["1000" ,
      "500",
      "100" ],
-    answer: 1
+    answer: "1000"
     },
 
     {question: "What did Itachi use to defeat kabuto" ,
     pick: ["Izanagi" ,
      "Tsukiyomi",
      "Izanami"] ,
-    answer: 3,
+    answer:  "Izanami"
     }
 
 
@@ -30,11 +30,13 @@ var time = document.querySelector(".timer");
 var questionsEl = document.querySelector(".questions");
 var questionText = document.querySelector("#question");
 var questionNum =document.querySelector("#num");
+var results = document.querySelector('.results')
 
 
 var choice1 = document.querySelector("#choice1");
 var choice2 = document.querySelector("#choice2");
 var choice3 = document.querySelector("#choice3");
+var highscore = document.querySelector('#results')
 
 var mcs = document.querySelector(".mc");
 
@@ -69,26 +71,38 @@ function quiz(){
     choice2.textContent = quest[index].pick[1];
     choice3.textContent = quest[index].pick[2];
 
+    choice1.addEventListener('click', checkAnswer)
+    choice2.addEventListener('click', checkAnswer)
+    choice3.addEventListener('click', checkAnswer)
+    
+
 }
 
-function checkAnswer () {
+function checkAnswer() {
+if(this.textContent === quest[index].answer) {
+    timer += 5; // if the answer is correct increase the time by 5 seconds
+} else {
+    timer -= 5; // if not decrease the time by 5 seconds
+} if (timer <= 0) endQuiz(); // if the timer is less than or equal to 0 end the quiz
+    index++; // increment the questions
+if(index >= quest.length) {
+    endQuiz();
+} else{
+    quiz();
+}
+}
 
-    if (quest[index].answer === quest[index].pick[answer]) {
+function endQuiz(){
+    // asking for username so that the score can be saved
+    quizContent.style.display = "none"
+    results.style.display = "flex"
 
-        right++;
+    let userName = prompt('Please enter your username')
 
-    } else {
-        
-        timer -= 5;
-        timeLeft.textContent = timer;
+    localStorage.setItem('highscore', JSON.stringify({userName: userName, score: timer}));
 
-    }
+    highscore.textContent = localStorage.getItem('highscore', JSON.stringify({userName: userName, score: timer}))
 
-    index++;
+    
 
-    if (index < quest.length) {
-        quiz();
-    } else {
-        wellDone();
-    }
 }
